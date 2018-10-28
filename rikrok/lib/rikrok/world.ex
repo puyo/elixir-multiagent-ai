b24f555d8493175212c7bc781212ccc55aff7bb5
Gregory McIntyre <greg@gregorymcintyre.com>
2018-10-28 14:04:24 +1100

    Group world server code better

--- rikrok/lib/rikrok/world.ex
+++ rikrok/lib/rikrok/world.ex
@@ -44,8 +44,8 @@ defmodule Rikrok.World do
     key: %{
       "." => %Ground{},
       "#" => %Wall{},
-      "@" => %Ground{mob: %Person{name: "Greg", colour: 0x0080ff}},
-      "*" => %Ground{mob: %Person{name: "Ruby", colour: 0xff0000}},
+      "@" => %Ground{mob: %Person{name: "Greg", colour: 0x0080FF}},
+      "*" => %Ground{mob: %Person{name: "Ruby", colour: 0xFF0000}}
     }
   }
 
@@ -53,10 +53,68 @@ defmodule Rikrok.World do
   # client
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, initial_state(), name: __MODULE__)
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def initial_state do
+  # --------------------------------------------------
+  # server
+
+  def init(_) do
+    Process.send_after(self(), :send_to_viewer, 200)
+    Process.send_after(self(), :start_mobs, 500)
+    {:ok, initial_state()}
+  end
+
+  def handle_call({:move_mob, mob, dx, dy}, _from, state) do
+    # IO.inspect "World move mob"
+    new_mob =
+      mob
+      |> set_pos(mob.x + dx, mob.y + dy)
+
+    if did_move(dx, dy) && in_world(new_mob.x, new_mob.y, state) && can_move_to(new_mob.x, new_mob.y, state) do
+      new_matrix =
+        state.matrix
+        |> update_in([new_mob.y, new_mob.x], fn t -> struct(t, mob: new_mob) end)
+        |> update_in([mob.y, mob.x], fn t -> struct(t, mob: nil) end)
+
+      new_state =
+        state
+        |> put_in([:matrix], new_matrix)
+
+      {:reply, new_mob, new_state}
+    else
+      {:reply, mob, state}
+    end
+  end
+
+  def handle_info(:send_to_viewer, state) do
+    GenServer.cast(Rikrok.ViewerProxy, {:send, matrix_to_glyph_map(state.matrix)})
+    Process.send_after(self(), :send_to_viewer, 100)
+    {:noreply, state}
+  end
+
+  def handle_info(:start_mobs, state) do
+    state
+    |> mobs()
+    |> Enum.each(fn m -> Rikrok.MobSupervisor.start_child(m) end)
+
+    {:noreply, state}
+  end
+
+  defp did_move(dx, dy) do
+    dx != 0 || dy != 0
+  end
+
+  defp can_move_to(x, y, state) do
+    t = get_in(state.matrix, [y, x])
+    is_nil(t.mob) && !t.blocker
+  end
+
+  defp in_world(x, y, state) do
+    0 <= x && x < state.width - 1 && 0 <= y && y < state.height - 1
+  end
+
+  defp initial_state do
     layout =
       @world.layout
       |> String.trim()
@@ -99,7 +157,7 @@ defmodule Rikrok.World do
     |> Tensor.Matrix.to_list()
     |> Enum.map(fn row ->
       Enum.map(row, fn terrain ->
-        Tuple.to_list glyph_for(terrain)
+        Tuple.to_list(glyph_for(terrain))
       end)
     end)
   end
@@ -121,63 +179,4 @@ defmodule Rikrok.World do
   defp glyph_for(%{mob: %struct{} = mob}), do: struct.glyph(mob)
   defp glyph_for(%{items: [%struct{} = item | _]}), do: struct.glyph(item)
   defp glyph_for(%struct{} = terrain), do: struct.glyph(terrain)
-
-  # --------------------------------------------------
-  # server
-
-  def init(state) do
-    Process.send_after(self(), :send_to_viewer, 200)
-    Process.send_after(self(), :start_mobs, 500)
-    {:ok, state}
-  end
-
-  def handle_call({:move_mob, mob, dx, dy}, _from, state) do
-    #IO.inspect "World move mob"
-    new_mob =
-      mob
-      |> set_pos(mob.x + dx, mob.y + dy)
-
-    if did_move(dx, dy) && in_world(new_mob.x, new_mob.y, state) && can_move_to(new_mob.x, new_mob.y, state) do
-      new_matrix =
-        state.matrix
-        |> update_in([new_mob.y, new_mob.x], fn t -> struct(t, mob: new_mob) end)
-        |> update_in([mob.y, mob.x], fn t -> struct(t, mob: nil) end)
-
-      new_state =
-        state
-        |> put_in([:matrix], new_matrix)
-
-      {:reply, new_mob, new_state}
-    else
-      {:reply, mob, state}
-    end
-  end
-
-  def did_move(dx, dy) do
-    dx != 0 || dy != 0
-  end
-
-  def can_move_to(x, y, state) do
-    t = get_in(state.matrix, [y, x])
-    is_nil(t.mob) && !t.blocker
-  end
-
-  def in_world(x, y, state) do
-    0 <= x && x < (state.width - 1) &&
-      0 <= y && y < (state.height - 1)
-  end
-
-  def handle_info(:send_to_viewer, state) do
-    GenServer.cast(Rikrok.ViewerProxy, {:send, matrix_to_glyph_map(state.matrix)})
-    Process.send_after(self(), :send_to_viewer, 100)
-    {:noreply, state}
-  end
-
-  def handle_info(:start_mobs, state) do
-    state
-    |> mobs()
-    |> Enum.each(fn m -> Rikrok.MobSupervisor.start_child(m) end)
-
-    {:noreply, state}
-  end
 end
