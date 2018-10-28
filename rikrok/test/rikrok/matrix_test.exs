e8d68f83a38999ee70db0b069a5d00ab590aed78
Gregory McIntyre <greg@gregorymcintyre.com>
2018-10-28 15:46:41 +1100

    More matrix tests

--- rikrok/test/rikrok/matrix_test.exs
+++ rikrok/test/rikrok/matrix_test.exs
@@ -2,9 +2,16 @@ defmodule Rikrok.MatrixTest do
   use ExUnit.Case
 
   test "sub_matrix" do
-    matrix = Tensor.Matrix.new([[1,2], [3,4]], 2, 2)
+    matrix = Tensor.Matrix.new([[1, 2], [3, 4]], 2, 2)
     result = Rikrok.Matrix.sub_matrix(matrix, -10, -10, 20, 20)
     expected = result
-    assert matrix == expected
+    assert result == expected
+  end
+
+  test "flat_map" do
+    matrix = Tensor.Matrix.new([[1, 2], [3, 4]], 2, 2)
+    result = Rikrok.Matrix.flat_map(matrix, fn x -> x end)
+    expected = [1, 2, 3, 4]
+    assert result == expected
   end
 end
