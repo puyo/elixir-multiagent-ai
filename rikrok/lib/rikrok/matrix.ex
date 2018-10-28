8b678aa8842719fdb9c970b36e0b9123f6018a70
Gregory McIntyre <greg@gregorymcintyre.com>
2018-10-28 16:26:57 +1100

    This function is hateful... rewrite it

--- rikrok/lib/rikrok/matrix.ex
+++ rikrok/lib/rikrok/matrix.ex
@@ -1,30 +1,31 @@
 defmodule Rikrok.Matrix do
   def sub_matrix(matrix, x, y, w, h) do
-    ww = Tensor.Matrix.width(matrix)
-    wh = Tensor.Matrix.height(matrix)
+    # ww = Tensor.Matrix.width(matrix)
+    # wh = Tensor.Matrix.height(matrix)
 
     xmin = Enum.max(0..x)
     ymin = Enum.max(0..y)
-    xmax = Enum.min((x + w)..ww) - 1
-    ymax = Enum.min((y + h)..wh) - 1
 
-    rw = xmax - xmin + 1
-    rh = ymax - ymin + 1
+    xmax = x + w - 1
+    ymax = x + h - 1
 
     new_list =
       Enum.map(ymin..ymax, fn j ->
         Enum.map(xmin..xmax, fn i ->
-          val = matrix[j][i]
-          if val == 0 do
-            IO.inspect matrix: matrix, val: 0, j: j, i: i
-            raise "Uh oh"
-          else
-            val
-          end
+          matrix[j][i]
         end)
+        |> Enum.reject(&is_nil/1)
       end)
+      |> Enum.reject(fn row -> length(row) == 0 end)
 
-    Tensor.Matrix.new(new_list, rw, rh)
+    nw = length(List.first(new_list) || [])
+    nh = length(new_list)
+
+    if nw > 0 && nh > 0 do
+      Tensor.Matrix.new(new_list, nw, nh)
+    else
+      nil
+    end
   end
 
   def flat_map(matrix, f) do
@@ -34,10 +35,10 @@ defmodule Rikrok.Matrix do
     Enum.flat_map(0..(h - 1), fn j ->
       Enum.map(0..(w - 1), fn i ->
         x = matrix[j][i]
-        if x == 0 do
-          IO.inspect i: i, j: j, matrix: matrix
-          raise "Should not be 0: i = #{i} j = #{j}, #{inspect matrix}"
-        end
+        # if x == 0 do
+        #   IO.inspect i: i, j: j, matrix: matrix
+        #   raise "Should not be 0: i = #{i} j = #{j}, #{inspect matrix}"
+        # end
         f.(x)
       end)
     end)
--- rikrok/test/rikrok/matrix_test.exs
+++ rikrok/test/rikrok/matrix_test.exs
@@ -1,17 +1,54 @@
 defmodule Rikrok.MatrixTest do
   use ExUnit.Case
 
-  test "sub_matrix" do
-    matrix = Tensor.Matrix.new([[1, 2], [3, 4]], 2, 2)
-    result = Rikrok.Matrix.sub_matrix(matrix, -10, -10, 20, 20)
-    expected = result
-    assert result == expected
+  setup do
+    matrix =
+      Tensor.Matrix.new(
+        [
+          [1, 2, 3, 4],
+          [11, 12, 13, 14],
+          [21, 22, 23, 24],
+          [31, 32, 33, 34]
+        ],
+        4,
+        4
+      )
+
+    {:ok, matrix: matrix}
   end
 
-  test "flat_map" do
-    matrix = Tensor.Matrix.new([[1, 2], [3, 4]], 2, 2)
-    result = Rikrok.Matrix.flat_map(matrix, fn x -> x end)
-    expected = [1, 2, 3, 4]
-    assert result == expected
+  describe "sub_matrix" do
+    test "big", %{matrix: matrix} do
+      result = Rikrok.Matrix.sub_matrix(matrix, -10, -10, 20, 20)
+      expected = matrix
+      assert result == expected
+    end
+
+    test "small", %{matrix: matrix} do
+      result = Rikrok.Matrix.sub_matrix(matrix, 1, 1, 2, 2)
+      expected = Tensor.Matrix.new([[12, 13], [22, 23]], 2, 2)
+      assert result == expected
+    end
+
+    test "1x1", %{matrix: matrix} do
+      result = Rikrok.Matrix.sub_matrix(matrix, 1, 1, 1, 1)
+      expected = Tensor.Matrix.new([[12]], 1, 1)
+      assert result == expected
+    end
+
+    test "clipped", %{matrix: matrix} do
+      result = Rikrok.Matrix.sub_matrix(matrix, 20, 20, 1, 1)
+      expected = nil
+      assert result == expected
+    end
+  end
+
+  describe "flat_map" do
+    test "basics" do
+      matrix = Tensor.Matrix.new([[1, 2], [3, 4]], 2, 2)
+      result = Rikrok.Matrix.flat_map(matrix, fn x -> x end)
+      expected = [1, 2, 3, 4]
+      assert result == expected
+    end
   end
 end
